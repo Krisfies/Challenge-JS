2df0a889868b831546ac08dd1f4e97551b0dc173
AlexandreRocchi <90569500+AlexandreRocchi@users.noreply.github.com>
2022-05-30 15:45:04 +0200

    Add files via upload

---
+++ Exercice 2/main.js
@@ -0,0 +1,4 @@
+// Fait touner la corne de feu avec GSAP
+TweenMax.to('.corne',5,{repeat:-1,backgroundPosition:"0px -46680px",ease:SteppedEase.config(120)});
+// Fait touner le cube avec GSAP
+TweenMax.to('.cube',5,{repeat:-1,backgroundPosition:"0px -39864px",ease:SteppedEase.config(132)});
